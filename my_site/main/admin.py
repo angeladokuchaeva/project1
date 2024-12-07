982a00ad62b278b33af7cbec47a9f5a29d0b279f
Angela <angela.595@icloud.com>
2024-12-07 03:54:23 +0300

    feat(admin): add custom user admin

--- my_site/main/admin.py
+++ my_site/main/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from main.models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # Настраиваем отображение в списке
+    list_display = ('user_id', 'username', 'is_active')
+    search_fields = ('username',)
+    ordering = ('date_joined',)
+
+    # Настраиваем формы редактирования
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'is_staff', 'is_active'),
+        }),
+    )
