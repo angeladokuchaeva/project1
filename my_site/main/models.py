ebe47c66e8b02c8a85e9aaf48427ab6b1b3e1698
Angela <angela.595@icloud.com>
2024-12-07 03:36:58 +0300

    feat(models): remove username field in user model

--- my_site/main/models.py
+++ my_site/main/models.py
@@ -5,7 +5,6 @@ from django.db import models
 
 class User(AbstractUser):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username = models.CharField(max_length=150, unique=True)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
