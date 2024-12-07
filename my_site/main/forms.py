7b766c553d498c68e522e48d989625490b540985
Angela <angela.595@icloud.com>
2024-12-07 04:10:48 +0300

    feat(forms): add LoginForm

--- my_site/main/forms.py
+++ my_site/main/forms.py
@@ -1,9 +1,15 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from main.models import User
 
 
 class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+
+class LoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ['username', 'password']
