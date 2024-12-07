fc4b52e6f60bb19b5ce033dcc115578821aa66f8
Angela <angela.595@icloud.com>
2024-12-07 04:09:45 +0300

    feat(forms): add RegisterForm

--- my_site/main/forms.py
+++ my_site/main/forms.py
@@ -1,14 +1,9 @@
-from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
 from main.models import User
 
 
-class UserCreationForm(forms.ModelForm):
+class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password']
-
-    def clean_username(self):
-        username = self.cleaned_data.get('username')
-        if User.objects.filter(username=username).exists():
-            raise forms.ValidationError("Username already exists.")
-        return username
